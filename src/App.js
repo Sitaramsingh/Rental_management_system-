9f14b63697f6f00a5c3d7d130e656fe4cd612ca9
Sita Ram <Sitaram@monocept.com>
2021-04-25 14:48:07 +0530

    commented code remove

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import  React from 'react';
-// import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
-// import Home from './Container/Home';
 import Routes from './Container/routes';
 
 function App() {
